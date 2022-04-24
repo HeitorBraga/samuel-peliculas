34e89d9e751b7e95e774c97fa58e2e759cc355dc
HeitorBraga <heitorbrasil62@gmail.com>
2022-04-24 11:01:53 -0300

    remove a fixed icon

--- components/FixedIcons.js
+++ components/FixedIcons.js
@@ -9,12 +9,6 @@ const FixedIcons = () => {
 					<figure className="image mr-3"><Image src="/whatsapp-icon.png" width="64px" height="64px" className="is-rounded" /></figure>
 				</a>
 			</Link>
-			
-			<Link href="tel:5562985836693">
-				<a className="is-hidden-mobile" href="tel:5562985836693">
-					<figure className="image mr-3"><Image src="/phone-icon.png" width="64px" height="64px" className="is-rounded" /></figure>
-				</a>
-			</Link>
 		</div>
 	)
 }
